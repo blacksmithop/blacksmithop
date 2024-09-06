53b9f1ecf5033b3873ebc6b49fa32e0dbeb1ad06
Abhinav <60320192+blacksmithop@users.noreply.github.com>
2024-09-06 09:37:17 +0000

    feat: Convert TODO to issues

--- app/main.py
+++ app/main.py
@@ -51,7 +51,8 @@ async def github_stats():
 @app.get("/githubRepos")
 async def github_repo_stats():
     repos = []
-    
+    # TODO Filter out response
+    # Return only necessary properties
     for i in range(1, 4):
         result = api.repos.list_for_user(username=USERNAME, per_page=100, page=i)
         result = list(result) # fastcore.foundation.L -> List
