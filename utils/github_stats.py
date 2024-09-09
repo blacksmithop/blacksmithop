d77b1242e9b77d597280f1bea95f4598810ec672
Abhinav KM <abhinav.m@zerone-consulting.com>
2024-09-09 17:39:47 +0530

    feat: Formatting

--- utils/github_stats.py
+++ utils/github_stats.py
@@ -8,7 +8,23 @@ USERNAME = getenv("GITHUB_USERNAME") or "blacksmithop"
 
 api = GhApi()
 
-ALLOWED_REPO_KEYS = ["name", "full_name", "description", "fork", "created_at", "updated_at", "pushed_at", "url", "homepage", "size", "stargazers_count", "watchers_count", "language", "forks_count"]
+ALLOWED_REPO_KEYS = [
+    "name",
+    "full_name",
+    "description",
+    "fork",
+    "created_at",
+    "updated_at",
+    "pushed_at",
+    "url",
+    "homepage",
+    "size",
+    "stargazers_count",
+    "watchers_count",
+    "language",
+    "forks_count",
+]
+
 
 def get_github_projects():
     repos = []
@@ -22,19 +38,39 @@ def get_github_projects():
             repos.extend(result)
         else:
             break
-        
+
     filtered_repos = []
     for repo in repos:
-        repo = {k:v  for k,v in repo.items() if k in ALLOWED_REPO_KEYS}
+        repo = {k: v for k, v in repo.items() if k in ALLOWED_REPO_KEYS}
         repo["url"] = repo["url"].replace("api.", "").replace("repos/", "")
         filtered_repos.append(repo)
-        
-    sorted_repos = sorted(filtered_repos, key=lambda d: d["stargazers_count"], reverse=True)
+
+    sorted_repos = sorted(
+        filtered_repos, key=lambda d: d["stargazers_count"], reverse=True
+    )
     return sorted_repos
 
-ALLOWED_PROFILE_KEYS = ["avatar_url", "name", "company", "blog", "location", "bio", "twitter_username", "public_repos", "public_gists", "followers", "following", "created_at", "updated_at"]
+
+ALLOWED_PROFILE_KEYS = [
+    "avatar_url",
+    "name",
+    "company",
+    "blog",
+    "location",
+    "bio",
+    "twitter_username",
+    "public_repos",
+    "public_gists",
+    "followers",
+    "following",
+    "created_at",
+    "updated_at",
+]
+
+
 def get_profile_data():
-    raw_profile_data =  api.users.get_by_username("blacksmithop")
-    profile_data = {k:v for k,v in raw_profile_data.items() if k in ALLOWED_PROFILE_KEYS}
+    raw_profile_data = api.users.get_by_username("blacksmithop")
+    profile_data = {
+        k: v for k, v in raw_profile_data.items() if k in ALLOWED_PROFILE_KEYS
+    }
     return profile_data
-    
