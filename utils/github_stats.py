d5a9a1b2dcda3e1b970b0259375b6f40035a7763
Abhinav KM <abhinav.m@zerone-consulting.com>
2024-09-09 17:02:10 +0530

    feat: Filter items in response data

--- utils/github_stats.py
+++ utils/github_stats.py
@@ -13,7 +13,7 @@ def get_github_projects():
     repos = []
     # TODO Filter out response
     # Return only necessary properties
-    for i in range(1, 4):
+    for i in range(1, 5):
         result = api.repos.list_for_user(username=USERNAME, per_page=100, page=i)
         result = list(result)  # fastcore.foundation.L -> List
         result_len = len(result)
@@ -23,5 +23,9 @@ def get_github_projects():
         else:
             break
 
+ALLOWED_KEYS = ["avatar_url", "name", "company", "blog", "location", "bio", "twitter_username", "public_repos", "public_gists", "followers", "following", "created_at", "updated_at"]
 def get_profile_data():
-    return api.users.get_by_username("blacksmithop")
+    raw_profile_data =  api.users.get_by_username("blacksmithop")
+    profile_data = {k:v for k,v in raw_profile_data.items() if k in ALLOWED_KEYS}
+    return profile_data
+    
