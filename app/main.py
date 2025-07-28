d00d995a060bd86f60b548b23a0e1a5ac7ebd60e
Abhinav KM <abhinav.km@thoughtminds.io>
2025-07-28 11:43:09 +0530

    feat: Update routers

--- app/main.py
+++ app/main.py
@@ -2,10 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware import Middleware
 from app.middleware.logging_middleware import LoggingMiddleware
-from app.endpoints import github_repos, static_files, tech_stack, contact, services
-from dotenv import load_dotenv
+from app.endpoints import github_repos, static_files, tech_stack, contact, services, docker
 
-load_dotenv()
 middleware = [
     Middleware(LoggingMiddleware),
     Middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"]),
@@ -18,3 +16,4 @@ app.include_router(static_files.router)
 app.include_router(tech_stack.router)
 app.include_router(contact.router)
 app.include_router(services.router)
+app.include_router(docker.router)
