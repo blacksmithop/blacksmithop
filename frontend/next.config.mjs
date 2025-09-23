c3d0915467242c8a61bd42027a381263cb7b74ba
Abhinav <60320192+blacksmithop@users.noreply.github.com>
2025-09-23 11:04:01 +0530

    fix: Unterminated comment

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} **/
 const nextConfig = {
   output: 'export', // Enables static export
   basePath: '/blacksmithop', // Matches your GitHub Pages repo path
